7e6500d90fb58a8c6df3d56029359fc14601b2ed
Josh Butcher <sjwb1999@gmail.com>
2025-06-26 16:02:45 -0700

    update HTML and JS files.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Business Logic for Pizza
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+}
+
+// Business Logic for Pizza Object
+
+// Business Logic
+
+// UI Logic
+
+
+window.addEventListener("load", function(){
+  let pizzaOrder = new Pizza([], "");
+});
