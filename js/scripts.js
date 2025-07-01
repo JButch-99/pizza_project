b618dc94a2ed20c3d9bbf4c9b544e21c59e6e366
Josh Butcher <sjwb1999@gmail.com>
2025-07-01 16:03:02 -0700

    update CSS, HTML, and JS files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,49 @@
 // Business Logic for Pizza
-function Pizza(topping, size) {
+function Pizza(topping, size, price) {
   this.topping = topping;
   this.size = size;
+  this.price = price;
 }
 
 // Business Logic for Pizza Object
 
+Pizza.prototype.updatePricing = function(charge) {
+  return this.price += charge;
+}
+
 // Business Logic
 
-// UI Logic
+function calculateAdditionalToppingTax(toppingArray) {
+  if (toppingArray.topping.length >= 4) {
+    let additionalToppingArray = toppingArray.slice(3);
+    for (let i = 0; i < additionalToppingArray.length; i++) {
+      toppingArray.updatePricing(1.75);
+    }
+  }
+}
 
+function calculatePizzaSizePrice(pizzaOrder) {
+  if (pizzaOrder.size === "Personal") {
+    pizzaOrder.updatePricing(5.99);
+  } else if (pizzaOrder.size === "Small") {
+    pizzaOrder.updatePricing(8.99);
+  } else if (pizzaOrder.size === "Medium") {
+    pizzaOrder.updatePricing(11.99);
+  } else if (pizzaOrder.size === "Large") {
+    pizzaOrder.updatePricing(14.99);
+  } else if (pizzaOrder.size === "Extra Large") {
+    pizzaOrder.updatePricing(17.99);
+  }
+}
+
+
+// UI Logic
+function handleSubmission(event, newPizzaOrder) {
+  event.preventDefault();
+  console.log(newPizzaOrder);
+} 
 
 window.addEventListener("load", function(){
-  let pizzaOrder = new Pizza([], "");
+  let newPizzaOrder = new Pizza([], "", "");
+  
 });
